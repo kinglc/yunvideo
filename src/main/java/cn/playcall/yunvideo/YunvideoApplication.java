1faab7d96d11c47435382e1cf82a5db3cd3cea44
Four5Fire <942558046@qq.com>
2018-12-12 23:29:22 +0800

    完成了小程序登录授权

---
+++ src/main/java/cn/playcall/yunvideo/YunvideoApplication.java
@@ -0,0 +1,14 @@
+package cn.playcall.yunvideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunvideoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YunvideoApplication.class, args);
+    }
+
+}
+
